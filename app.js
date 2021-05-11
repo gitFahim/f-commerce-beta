def9254a1e3513d79237d33ee4631addd7884e6f
Fahim <bsse0932@iit.du.ac.bd>
2021-05-11 16:50:05 +0600

    beta

---
+++ app.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
+
+//Set Views
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res) => {
+    res.render('index');
+});
+
+app.get('/hot', (req, res) => {
+    res.render('hot');
+});
+app.get('/blog', (req, res) => {
+    res.render('blog');
+});
+
+app.listen(5000, () => console.info('Listening on port 5000'));
