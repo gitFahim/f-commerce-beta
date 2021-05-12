52541d34db0dae210efbc7165711ba33f014c5bd
Fahim <bsse0932@iit.du.ac.bd>
2021-05-12 18:00:26 +0600

    express-ejs-layout added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const express = require('express');
+const expressLayout = require('express-ejs-layouts');
 
 const app = express();
 
@@ -7,12 +8,13 @@ app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/js', express.static(__dirname + 'public/js'));
 app.use('/img', express.static(__dirname + 'public/img'));
+app.use(expressLayout);
 
 //Set Views
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-app.get('/', (req,res) => {
+app.get('', (req,res) => {
     res.render('index');
 });
 
@@ -23,4 +25,11 @@ app.get('/blog', (req, res) => {
     res.render('blog');
 });
 
-app.listen(process.env.PORT || 5000);
+app.get('/home_made', (req, res) => {
+    res.render('home_made');
+});
+
+//app.listen(process.env.PORT || 5000);
+
+
+app.listen(5000, () => console.info('Listening on port 5000'));
